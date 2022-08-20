3d3ecd4a65d1dd27f2863ea8f18d96677ef0d271
Hillary Chemiron <chemiron.hillary@student.moringaschool.com>
2022-08-20 22:35:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName='bob';
+function upperCaseCustomerName(){
+    customerName.toUpperCase();
+    customerName.toUpperCase();
+}
+upperCaseCustomerName();
+function setBestCustomer(){
+    bestCustomer='not bob'
+}
+setBestCustomer();
+function overwriteBestCustomer(){
+    var bestCustomer='maybe bob';
+    return bestCustomer;
+}
+const changeLeastFavoriteCustomer='alan'
+function changeLeastFavoriteCustomer(){
+    const changeLeastFavoriteCustomer='lee';
+    return changeLeastFavoriteCustomer;
+}
